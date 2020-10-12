bab3185f8487f4aebc0339b987716afa982fb33f
Ensar Dogrusoz <kaandogrusoz00@gmail.com>
2020-10-12 02:01:15 -0400

    Updates

---
+++ precache-manifest.04880f24aad466b0e1f95e753dc8649a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a435130bc16fda666370e1b8a21bef",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "655968d6d1f4505f99cf",
+    "url": "/covid-19-tracker/static/css/main.e4908127.chunk.css"
+  },
+  {
+    "revision": "db6e2e6bbfea7c3920de",
+    "url": "/covid-19-tracker/static/js/2.3945e0c3.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19-tracker/static/js/2.3945e0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655968d6d1f4505f99cf",
+    "url": "/covid-19-tracker/static/js/main.d66c3adc.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
